1aa66346b6424ed4f8138fc2faf26dd5df19fbdf
dayakey <daya10key@gmail.com>
2022-11-18 19:37:49 +0600

    second commit

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import random
+choice = ["n", "k", "b"]
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def check_ans(t):
+    if t == "k":
+        print("kamen'")
+    elif t == "b":
+        print("bumaga")
+    elif t == "n":
+        print("nozhnicy")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def main():
+    while True:
+        turn = input("Enter your turn (k, n, b):").lower()
+        if turn == "exit":
+            break
+        else:
+            check_ans(turn)
+        c_turn = random.choice(choice)
+        check_ans(c_turn)
+        yours_count = 0
+        comp_count = 0
+        if c_turn == "n" and turn == "b" or c_turn == "b" and turn == "k" or c_turn == "k" and turn == "n":
+            print("Computer win")
+            comp_count += 1
+        elif turn == "n" and c_turn == "b" or turn == "b" and c_turn == "k" or turn == "k" and c_turn == "n":
+            print("You win")
+            yours_count += 1
+        elif turn == "n" and c_turn == "n" or turn == "b" and c_turn == "b" or turn == "k" and c_turn == "k":
+            print("Friendship")
+        print([yours_count, comp_count])
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+main()
